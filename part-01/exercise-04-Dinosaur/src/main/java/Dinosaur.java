13a7b5f5a48df4c1107361f73010b5641cb77dc8
shaaiapouf <erviana.205025@mhs.its.ac.id>
2022-07-26 11:09:53 +0700

    feat: solve exercise-04-Dinosaur

--- part-01/exercise-04-Dinosaur/src/main/java/Dinosaur.java
+++ part-01/exercise-04-Dinosaur/src/main/java/Dinosaur.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Dinosaur {
+public class Dinosaur {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
 }
