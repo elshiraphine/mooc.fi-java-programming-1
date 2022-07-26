993e3d25c2deea841d2b71c8d1ef83c2287bced9
shaaiapouf <erviana.205025@mhs.its.ac.id>
2022-07-26 11:09:21 +0700

    feat: solve exercise-03-OnceUponATime

--- part-01/exercise-03-Once-Upon-A-Time/src/main/java/OnceUponATime.java
+++ part-01/exercise-03-Once-Upon-A-Time/src/main/java/OnceUponATime.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class OnceUponATime {
+public class OnceUponATime {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
 }
